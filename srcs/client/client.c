e067c03cd3fb6c9ec32e1511cdd3d064ca90ead5
Loïc <lbertran@student.42lyon.fr>
2021-06-06 08:23:57 +0200

    working

--- srcs/client/client.c
+++ srcs/client/client.c
@@ -1,83 +1,72 @@
 #include "minitalk.h"
 
-int	ft_splitlen(char **split)
+void	send_signal(int pid, int signal)
 {
-	int	i;
-
-	i = 0;
-	if (!split)
-		return (0);
-	while (split[i])
-		i++;
-	return (i);
-}
-
-char	*compute_args(char **args)
-{
-	char	*ret;
-	char	*swap;
-	int		i;
-
-	i = 0;
-	if (ft_splitlen(args) == 0)
-		return (NULL);
-	ret = malloc(1);
-	*ret = 0;
-	while (args[++i])
+	if (signal == 0)
 	{
-		swap = ft_strjoin(ret, args[i]);
-		free(ret);
-		ret = swap;
-		swap = ft_strjoin(ret, " ");
-		free(ret);
-		ret = swap;
+		if (kill(pid, SIGUSR1) == -1)
+		{
+			printf("%sError: process with id %d doesn't exist.%s",
+				HRED, pid, RESET);
+			exit(1);
+		}
 	}
-	ret[ft_strlen(ret) - 1] = 0;
-	return (ret);
+	else if (signal == 1)
+	{
+		if (kill(pid, SIGUSR2) == -1)
+		{
+			printf("%sError: process with id %d doesn't exist.%s",
+				HRED, pid, RESET);
+			exit(1);
+		}
+	}
+	usleep(100);
 }
 
-int	main(int ac, char **av)
+void	msg_loop(char *msg, int pid)
 {
-	int		pid;
-	char	*msg;
 	char	*binary;
 	size_t	i;
 	size_t	j;
 
-	if (ac < 3)
-	{
-		printf("%sUsage: ./client <PID> <message>\n%s", HRED, RESET);
-		exit(1);
-	}
 	i = 0;
 	j = 0;
-	pid = ft_atoi(av[1]);
-	msg = compute_args(&av[1]);
 	while (msg[i])
 	{
 		binary = ascii_to_binary(msg[i]);
 		while (binary[j])
 		{
 			if (binary[j] == '0')
-			{
-				kill(pid, SIGUSR1);
-				usleep(50);
-			}
+				send_signal(pid, 0);
 			else if (binary[j] == '1')
-			{
-				kill(pid, SIGUSR2);
-				usleep(50);
-			}
+				send_signal(pid, 1);
 			j++;
 		}
+		free(binary);
 		j = 0;
 		i++;
 	}
-	/* while (j < 8) TODO null terminer le string pr tout print d'un coup au lieu de char par char
+}
+
+int	main(int ac, char **av)
+{
+	int		pid;
+	char	*msg;
+	int		i;
+
+	i = 0;
+	if (ac != 3)
+	{
+		printf("%sUsage: ./client <PID> <message>\n%s", HRED, RESET);
+		exit(1);
+	}
+	pid = ft_atoi(av[1]);
+	msg = av[2];
+	msg_loop(msg, pid);
+	while (i < 8)
 	{
 		kill(pid, SIGUSR1);
 		usleep(100);
-		j++;
-	} */
-	free(msg);
+		i++;
+	}
 }
--- srcs/server/server.c
+++ srcs/server/server.c
@@ -1,10 +1,49 @@
 #include "minitalk.h"
 
-void	append_nbr(char c)
+char	*strnew(char c)
+{
+	char	*ret;
+
+	ret = malloc (sizeof(char) * 2);
+	ret[0] = c;
+	ret[1] = 0;
+	return (ret);
+}
+
+void	append_byte(char c)
+{
+	static char	*msg;
+	char		*tmp;
+	char		char_str[2];
+
+	if (!c)
+	{
+		write(1, msg, ft_strlen(msg));
+		write(1, "\n", 1);
+		free(msg);
+		msg = NULL;
+	}
+	char_str[0] = c;
+	char_str[1] = 0;
+	//write(1, &c, 1);
+	if (!msg)
+		msg = strnew(c);
+	else
+	{
+		tmp = msg;
+		//printf("%c -> %s\n", c, tmp);
+		msg = ft_strjoin(tmp, char_str);
+		free(tmp);
+		//printf("new msg: %s\n", msg);
+	}
+}
+
+void	append_bit(char c)
 {
 	static char	*str;
 	int			chr;
 
+	//write(1, &c, 1);
 	if (!str)
 	{
 		str = malloc(sizeof(char) * 9);
@@ -16,7 +55,8 @@ void	append_nbr(char c)
 	if (ft_strlen(str) == 8)
 	{
 		chr = ft_atoi_base(str, "01");
-		write(1, &chr, 1);
+		append_byte(chr);
+		//write(1, &chr, 1);
 		free(str);
 		str = NULL;
 	}
@@ -25,9 +65,9 @@ void	append_nbr(char c)
 void	handle_signal(int id)
 {
 	if (id == SIGUSR1)
-		append_nbr('0');
+		append_bit('0');
 	else if (id == SIGUSR2)
-		append_nbr('1');
+		append_bit('1');
 }
 
 int	main(void)
